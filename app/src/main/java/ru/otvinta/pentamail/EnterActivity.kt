c9f72f789e7b4b531d7b1dd7e3ad200c8c957657
entaro <apan.kirill@yandex.ru>
2019-03-13 20:03:41 +0300

    Update1.0

--- app/src/main/java/ru/otvinta/pentamail/EnterActivity.kt
+++ app/src/main/java/ru/otvinta/pentamail/EnterActivity.kt
@@ -3,10 +3,10 @@ package ru.otvinta.pentamail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class EnterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_enter)
     }
 }
