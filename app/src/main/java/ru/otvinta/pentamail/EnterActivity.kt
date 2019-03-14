c144f25d26f8b9ecca9163dd2d1b1b8cc62ff323
entaro <apan.kirill@yandex.ru>
2019-03-14 21:05:08 +0300

    Interface is ready(only enter)

--- app/src/main/java/ru/otvinta/pentamail/EnterActivity.kt
+++ app/src/main/java/ru/otvinta/pentamail/EnterActivity.kt
@@ -2,11 +2,14 @@ package ru.otvinta.pentamail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.textfield.TextInputEditText
 
 class EnterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter)
+
+
     }
 }
